f8a16057b481e88c1bb0eed0be74dee5fe8984a2
alison-campion <alicampion@gmail.com>
2020-06-25 13:51:00 -0400

    make custome front_matter tex file for como appendix

---
+++ como/tex/make-appendix.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+date
+
+echo "MAKE: FIRST LATEX RUN"
+pdflatex appendix.tex
+
+echo "MAKE: RUNNING BIBTEX"
+bibtex appendix.aux
+
+# now two pdflatex to include references
+echo "MAKE: 2ND LATEX RUN"
+pdflatex appendix.tex
+
+echo "MAKE: 3RD LATEX RUN"
+pdflatex appendix.tex
+
