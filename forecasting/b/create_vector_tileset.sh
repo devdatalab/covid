269f97b7c5ba77088e93929d7782e5e0d74a53dd
Tobias Lunt <toby.lunt@gmail.com>
2021-05-06 17:54:54 -0400

    Add dist boundary simplification via tippecanoe

--- forecasting/b/create_vector_tileset.sh
+++ forecasting/b/create_vector_tileset.sh
@@ -10,4 +10,4 @@
 # format may change in the future."
 
 # create district tileset with zoom range defined (cost saver)
-~/iec/local/share/tippecanoe/tippecanoe --force -z9 -Z5 -o $TMP/covid_data.mbtiles --read-parallel --coalesce-smallest-as-needed --detect-shared-borders --generate-ids $IEC/covid/forecasting/district.geojson
+~/iec/local/share/tippecanoe/tippecanoe --force -z9 -Z5 -o $TMP/covid_data.mbtiles --read-parallel --simplification=10 --coalesce-smallest-as-needed --detect-shared-borders --generate-ids $IEC/covid/forecasting/district.geojson
